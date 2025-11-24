7efd4931a651fb76aea0ce079c7010832efefff9
Alex Baker <iamalexbaker@gmail.com>
2025-11-24 15:52:22 +0700

    ye_olde_todos + checksum script

--- Formula/simcleaner.rb
+++ Formula/simcleaner.rb
@@ -2,7 +2,7 @@ class Simcleaner < Formula
   desc "CLI tool for cleaning ios simulator runtimes"
   homepage "https://github.com/goodpals/simcleaner"
   url "https://github.com/goodpals/simcleaner/archive/refs/tags/v0.1.0.tar.gz"
-  license "APGL-3.0"
+  license "AGPL-3.0"
   head "https://github.com/goodpals/simcleaner.git", branch: "main"
   version "0.1.0"
   sha256 "c793eab7018d59005a7679dd1dba8e6cab8660eb3189654909907c84ebf0b46a"
