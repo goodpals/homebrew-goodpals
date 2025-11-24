e7948e60500317d19d204389d4ed08fc93f0541c
Alex Baker <iamalexbaker@gmail.com>
2025-11-24 15:53:59 +0700

    i am stupid

--- Formula/ye_olde_todos.rb
+++ Formula/ye_olde_todos.rb
@@ -1,9 +1,9 @@
 class YeOldeTodos < Formula
   desc "Tool for finding todo comments in code and sorting them by age"
-  homepage "https://github.com/goodpals/ye-olde-todos"
-  url "https://github.com/goodpals/ye-olde-todos/archive/refs/tags/v0.1.0.tar.gz"
+  homepage "https://github.com/goodpals/ye_olde_todos"
+  url "https://github.com/goodpals/ye_olde_todos/archive/refs/tags/v0.1.0.tar.gz"
   license "AGPL-3.0"
-  head "https://github.com/goodpals/ye-olde-todos.git", branch: "main"
+  head "https://github.com/goodpals/ye_olde_todos.git", branch: "main"
   version "0.1.0"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
