a73297a1ebf2a47967f3a06d51ffb853e7e276eb
Alex Baker <iamalexbaker@gmail.com>
2025-11-24 16:23:10 +0700

    ye_olde_todos 0.1.1

--- Formula/ye_olde_todos.rb
+++ Formula/ye_olde_todos.rb
@@ -1,11 +1,11 @@
 class YeOldeTodos < Formula
   desc "Tool for finding todo comments in code and sorting them by age"
   homepage "https://github.com/goodpals/ye_olde_todos"
-  url "https://github.com/goodpals/ye_olde_todos/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/goodpals/ye_olde_todos/archive/refs/tags/v0.1.1.tar.gz"
   license "AGPL-3.0"
   head "https://github.com/goodpals/ye_olde_todos.git", branch: "main"
-  version "0.1.0"
-  sha256 "3eeae8529f91c19025336aabba86a6db7bcc8df5e25e7b7f3997d6900ad68eaf"
+  version "0.1.1"
+  sha256 "3197d9d3ca43936344013f601de9e6702892382aefea434805b1db19c08f378b"
 
   depends_on "rust" => :build
 
