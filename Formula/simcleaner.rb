fd26397a20ec219585b807cbbbade1d3dada9f5e
Alex Baker <iamalexbaker@gmail.com>
2026-07-19 22:18:22 +0700

    fix stuff

--- Formula/simcleaner.rb
+++ Formula/simcleaner.rb
@@ -2,10 +2,9 @@ class Simcleaner < Formula
   desc "CLI tool for cleaning ios simulator runtimes"
   homepage "https://github.com/goodpals/simcleaner"
   url "https://github.com/goodpals/simcleaner/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "5f55a7c7a72e6fe2c909c86f59f774a47a274d30c23456cce88ccca226f47999"
   license "AGPL-3.0"
   head "https://github.com/goodpals/simcleaner.git", branch: "main"
-  version "0.2.0"
-  sha256 "5f55a7c7a72e6fe2c909c86f59f774a47a274d30c23456cce88ccca226f47999"
 
   depends_on "rust" => :build
 
@@ -13,5 +12,5 @@ class Simcleaner < Formula
     system "cargo", "install", *std_cargo_args
   end
 
-  # todo: test
+  # TODO: test
 end
--- Formula/yot.rb
+++ Formula/yot.rb
@@ -2,10 +2,9 @@ class Yot < Formula
   desc "Ye Olde Todos: tool for finding todo comments in code and sorting them by age"
   homepage "https://github.com/goodpals/ye_olde_todos"
   url "https://github.com/goodpals/ye_olde_todos/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "80d6dfa25088da88aa6c058f432ede7cdbaf27836dba3caa68f73cc1a16dae41"
   license "AGPL-3.0"
   head "https://github.com/goodpals/ye_olde_todos.git", branch: "main"
-  version "1.2.0"
-  sha256 "80d6dfa25088da88aa6c058f432ede7cdbaf27836dba3caa68f73cc1a16dae41"
 
   depends_on "rust" => :build
 
@@ -13,7 +12,7 @@ class Yot < Formula
     system "cargo", "install", *std_cargo_args
   end
 
-  def test
-    system "cargo", "test"
+  test do
+    system bin/"yot", "--help"
   end
 end
